a7e4f32c26f1489fcf926c857addb60f1f644cdf
thefalked <giulainocrivelli@hotmail.com>
2021-03-12 22:34:30 -0400

    finishing taskList, all tests passing

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,39 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const randomId = parseInt(Math.random().toString().replace("0.", ""));
+
+    if (newTaskTitle.trim() !== "") {
+      const task = {
+        id: randomId,
+        title: newTaskTitle,
+        isComplete: false,
+      }
+
+      setNewTaskTitle("");
+
+      setTasks([...tasks, task]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+
+      return task;
+    });
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const taskRemoved = tasks.filter(task => task.id !== id);
+
+    setTasks(taskRemoved);
   }
 
   return (
